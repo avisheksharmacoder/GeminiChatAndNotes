05e1bcb1164d2b9bb85f7478e1a18040e3d16a47
avishekflash <avisheksharmacoder@gmail.com>
2025-02-03 01:23:37 +0530

    added code for Chat Class, generating tables using pony orm

--- src/gemini_chat_orm.py
+++ src/gemini_chat_orm.py
@@ -1,4 +1,5 @@
 from pony import orm
+from datetime import datetime
 
 
 # make Database Object.
@@ -7,4 +8,13 @@ db = orm.Database()
 # make database file, for binding classes into tables.
 db.bind(provider="sqlite", filename="./sqlite_db/chat.db", create_db=True)
 
+
 # make Schema class for storing chats as records.
+class Chat(db.Entity):
+    prompt = orm.Required(str)
+    prompt_response = orm.Required(str)
+    prompt_time = orm.Required(datetime)
+
+
+# generate the tables.
+db.generate_mapping(create_tables=True)
