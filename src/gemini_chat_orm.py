18856ef3ae6dc9125bb6e275973ab076c814fcb8
avishekflash <avisheksharmacoder@gmail.com>
2025-02-03 23:50:25 +0530

    updated comments for chat orm code

--- src/gemini_chat_orm.py
+++ src/gemini_chat_orm.py
@@ -2,19 +2,21 @@ from pony import orm
 from datetime import datetime
 
 
-# make Database Object.
+# We have to make Database object, to perform DB operations.
 db = orm.Database()
 
-# make database file, for binding classes into tables.
+# We have to bind database object, with db engine, file name and whether
+# to create the database file or not.
 db.bind(provider="sqlite", filename="./sqlite_db/chat.db", create_db=True)
 
 
-# make Schema class for storing chats as records.
+# We have to make the Schema class, that will let us store rows in
+# the database.
 class Chat(db.Entity):
     prompt = orm.Required(str)
     prompt_response = orm.Required(str)
     prompt_time = orm.Required(datetime)
 
 
-# generate the tables.
+# We have to generate the tables to store data.
 db.generate_mapping(create_tables=True)
