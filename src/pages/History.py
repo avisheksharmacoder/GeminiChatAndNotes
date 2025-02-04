54af3dc97fdc3c38f3d696f871ac24377ec74c54
avishekflash <avisheksharmacoder@gmail.com>
2025-02-05 01:19:51 +0530

    new changes

--- src/pages/History.py
+++ src/pages/History.py
@@ -2,7 +2,36 @@ import streamlit as st
 
 st.set_page_config(page_title="History", page_icon="👋")
 
-st.sidebar.success("Download your chat History")
+st.sidebar.success("Download your Chats")
+
+prompts = [
+    (0, "What is pectin? "),
+    (1, "What is protein? explain brief"),
+    (2, "What is fat ??"),
+]
+
+
+def delete_record(i):
+    return prompts.pop(i)
 
 
 st.header("Your Gemini Chat History")
+
+
+st.write("All your prompts and responses with Gemini will appear here !")
+
+
+# We show the prompts as checkboxes.
+# for p in prompts:
+#     expander = st.expander(p)
+#     expander.write(p + "This is the best I could come up with ")
+#     expander.button("Delete", key=p + str(len(p)))
+
+for p in prompts:
+    expander = st.expander(p[1])
+    expander.text_area(label="Edit answer", value=p[1] + "This is the besst I can do")
+    expander.checkbox("Choose to Download", key=p[0])
+
+
+# This button will download all chats into pdf format.
+st.button("Download")
