45cb73f393e3ae600fde8faecab4a7eba6252157
avishekflash <avisheksharmacoder@gmail.com>
2025-02-09 01:20:45 +0530

    added DB file exists checks

--- src/gemini_chat_orm.py
+++ src/gemini_chat_orm.py
@@ -1,13 +1,22 @@
 from pony import orm
 from datetime import datetime
+import os
+
+
+# check if sqlite db is in folder or not.
+def sqlite_db_exists(filename: str, folder_path: str) -> bool:
+    file_path = os.path.join(folder_path, filename)
+    return os.path.isfile(file_path)
 
 
 # We have to make Database object, to perform DB operations.
 db = orm.Database()
 
-# We have to bind database object, with db engine, file name and whether
-# to create the database file or not.
-db.bind(provider="sqlite", filename="./sqlite_db/chat.db", create_db=True)
+# This is the sqlite database file name.
+filename = "chat.db"
+
+# This is the folder path where the database file is stored.
+folder_path = "./sqlite_db"
 
 
 # We have to make the Schema class, that will let us store rows in
@@ -18,5 +27,12 @@ class Chat(db.Entity):
     prompt_time = orm.Required(datetime)
 
 
-# We have to generate the tables to store data.
-db.generate_mapping(create_tables=True)
+# We have to bind database object, with db engine, file name and whether
+# to create the database file or not.
+if not sqlite_db_exists(filename, folder_path):
+    db.bind(provider="sqlite", filename=folder_path + "/" + filename, create_db=True)
+    print("database file created !!")
+    # We have to generate the tables to store data.
+    db.generate_mapping(create_tables=True)
+else:
+    print("database already exists !!")
