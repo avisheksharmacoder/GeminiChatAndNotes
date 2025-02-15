822cb3fcede8ba3e58a83d90355491151d26b3b4
avishekflash <avisheksharmacoder@gmail.com>
2025-02-16 01:10:04 +0530

    added database file creation changes

--- src/GemApp2o.py
+++ src/GemApp2o.py
@@ -8,6 +8,45 @@ from pony import orm
 from fpdf import FPDF
 
 
+# We have to create a database file to store the prompts.
+# and responses. We will use pony orm for this purpose.
+# check if sqlite db is in folder or not.
+def sqlite_db_exists(filename: str, folder_path: str) -> bool:
+    file_path = os.path.join(folder_path, filename)
+    return os.path.isfile(file_path)
+
+
+# We have to make Database object, to perform DB operations.
+db = orm.Database()
+
+# This is the sqlite database file name.
+db_filename = "chat.db"
+
+# This is the folder path where the database file is stored.
+db_folder_path = "./sqlite_db"
+
+
+# We have to make the Schema class, that will let us store rows in
+# the database as a record.
+class Chat(db.Entity):
+    prompt = orm.Required(str)
+    prompt_response = orm.Required(str)
+    prompt_time = orm.Required(datetime)
+
+
+# We have to bind database object, with db engine, file name and whether
+# to create the database file or not.
+if not sqlite_db_exists(db_filename, db_folder_path):
+    db_file_location = db_folder_path + "/" + db_filename
+    db.bind(provider="sqlite", filename=db_file_location, create_db=True)
+
+    print("database file created !!")
+    # We have to generate the tables to store data.
+    db.generate_mapping(create_tables=True)
+else:
+    print("database already exists !!")
+
+
 # response text.
 gemini_response: str = ""
 
